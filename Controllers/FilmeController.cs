486cefafd879c7690cb3185eccb4bdd50d97bc8c
Anderson Viana <valladau2@gmail.com>
2021-07-22 18:16:02 -0300

    Iniciando Projeto
    Iniciando projeto FilmesAPI

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
