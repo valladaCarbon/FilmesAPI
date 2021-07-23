92ec66e790da844a68d4221aa830fe5de9898fe6
Anderson Viana <valladau2@gmail.com>
2021-07-23 14:16:31 -0300

    Configurando Persistência
    Configurando a Persistência no Banco de Dados

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FilmesAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace FilmesAPI
@@ -26,7 +21,9 @@ namespace FilmesAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<FilmeContext>(opts => opts.UseMySQL(
+                Configuration.GetConnectionString("FilmeConnection")
+            ));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
